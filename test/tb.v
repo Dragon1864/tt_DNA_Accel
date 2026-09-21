00d415d35ec05a2493e9da2be1192dc751ff6843
Suhani Das <95483813+Das2225@users.noreply.github.com>
2026-09-21 16:53:54 +0530

    Refactor testbench to include power ports for GL_TEST

--- test/tb.v
+++ test/tb.v
@@ -1,18 +1,15 @@
 `default_nettype none
 `timescale 1ns / 1ps
-
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
 module tb ();
-
   // Dump the signals to a FST file. You can view it with gtkwave or surfer.
   initial begin
     $dumpfile("tb.fst");
     $dumpvars(0, tb);
     #1;
   end
-
   // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
@@ -22,9 +19,18 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+`ifdef GL_TEST
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
+`endif
+  // Our DNA_Accel top module (clk here is sclk -- the external SPI clock,
+  // per the note in tt_um_dna_accel_new.v):
+  tt_um_das2225_dna_accel user_project (
+      // Include power ports for the Gate Level test:
+`ifdef GL_TEST
+      .VPWR(VPWR),
+      .VGND(VGND),
+`endif
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
@@ -34,5 +40,4 @@ module tb ();
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
-
 endmodule
