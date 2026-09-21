f662cb533f8e8821eee41904f9549e33a960d779
Garv R Jain <122778557+Dragon1864@users.noreply.github.com>
2026-09-21 17:22:13 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -19,18 +19,10 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
+
   // Our DNA_Accel top module (clk here is sclk -- the external SPI clock,
   // per the note in tt_um_dna_accel_new.v):
   tt_um_das2225_dna_accel user_project (
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
